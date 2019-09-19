50354e6525eb590801934559804f27ab908015eb
sabarihari <sabarihRI@97>
2019-09-19 14:40:33 +0530

    I committed the code

---
+++ src/org/pro/Sample.java
@@ -0,0 +1,19 @@
+package org.pro;
+
+public  class Sample {
+	public static void CompName()
+	{
+		System.out.println("Company name is CTS");
+	}
+	public static void EmpName()
+	{
+		System.out.println("Employee Name is Sabari");
+	}
+	public static void main(String[]args)
+	{
+		
+		CompName();
+		EmpName();
+	}
+
+}
